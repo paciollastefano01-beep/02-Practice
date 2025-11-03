779269e79e0aa2213b125ffdf8ea06c8ac412519
Stefano Paciolla <paciollastefano01@gmail.com>
2025-11-03 18:38:21 +0100

    add modifiche programma più pulito

--- week1-python-basics/Giorno 2/mini_progetto_giorno2.py
+++ week1-python-basics/Giorno 2/mini_progetto_giorno2.py
@@ -18,10 +18,12 @@ costo_totale_prima_sconto = costo_base + costo_totale_dipendenti
 
 if settore == "ristorazione":
     sconto = 0.20
-elif settore == "e-commerce":
+elif settore == "e-commerce" or settore == "ecommerce" or settore == "e commerce":   #ho aggiunto modifica per scrittura dell'utente
     sconto = 0.10
+elif settore == "servizi":
+    sconto = 0.0
 else:
-    sconto = 0
+    sconto = 0.0
 
 sconto_euro = costo_totale_prima_sconto * sconto
 costo_finale = costo_totale_prima_sconto - sconto_euro
@@ -32,6 +34,7 @@ if budget_mensile >= costo_finale:
 else:
     deficit = costo_finale - budget_mensile
     esito = f"❌ Budget insufficiente (mancano €{deficit:.2f})"
+percento = int(sconto * 100) #aggiunto percentuale sconto
 
 print("======================")
 print("PREVENTIVO BOT")
@@ -44,7 +47,7 @@ print("--------------")
 print(f"Costo base: €{costo_base:.2f}")
 print(f"Extra Dipendenti: €{costo_totale_dipendenti:.2f}")
 print(f"Subtotale: €{costo_totale_prima_sconto:.2f}")
-print(f"Sconto {settore}: -€{sconto_euro:.2f}")
+print(f"Sconto {settore.title()} ({percento}%): -€{sconto_euro:.2f}")
 print("--------------")
 print(f"TOTALE: €{costo_finale:.2f}")
 print(f"\nBudget: €{budget_mensile:.2f}")

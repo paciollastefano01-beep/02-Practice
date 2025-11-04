9e511177db66f656a9898e064477f2d324f7580e
Stefano Paciolla <paciollastefano01@gmail.com>
2025-11-04 21:31:23 +0100

    add mini project mini bot

---
+++ week1-python-basics/Giorno 2/Giorno 3/mini_progetto_finale_giorno_3.py
@@ -0,0 +1,6 @@
+#mini bot che chiede cose e suggerisce soluzioni
+
+stanchezza = input("Sei stanco? (Si/No)").strip().lower()
+fame = input("Hai fame) (Si/No)").strip().lower()
+uscita = input("Vuoi uscire? (Si/No)").strip().lower()
+budget = float(input("Budget a disposizione? ")).strip().lower()

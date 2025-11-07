568dee248e01aa3ea3024b554185c92c77974ffd
stefano <paciollastefano01@gmail.com>
2025-11-07 16:42:24 +0100

    Oggi ho imparato ad usare il ciclo while

---
+++ week1-python-basics/Giorno 4/flag_booleano.py
@@ -0,0 +1,17 @@
+numero_magico = 7
+indovinato = False
+
+while numero_magico <= 10 and not indovinato:
+    tentativo = int(input("Indovina: "))
+    if tentativo == numero_magico:
+       indovinato = True
+       print("Bravo!")
+    else:
+        print("Riprova!")
+
+if indovinato:
+   print("Hai vinto!")
+   tentativo= 1 
+   print("Sisitema oansjudkd")
+   if tentativo = 1 :
+tentareeiorejerieoeireriofdujfjdfd

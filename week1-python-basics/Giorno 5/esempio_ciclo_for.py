3d5c6cacd3166731df04d63040a55c2815477999
stefano <paciollastefano01@gmail.com>
2025-11-11 11:54:24 +0100

    add exercise tabellina del 2, e chiede tabellina successiva mostrando la tabellina che ha scelto l'utente

---
+++ week1-python-basics/Giorno 5/esempio_ciclo_for.py
@@ -0,0 +1,14 @@
+print("TABELLINA DEL 2")
+print("-" * 15)  # trucco: stampa 15 trattini
+
+for i in range(1, 11):  # da 1 a 10
+    risultato = 2 * i  # calcola 2 * i
+    print(f"2 x {i} = {risultato}")
+
+tabellina = input("Quale tabellina vuoi? ")
+tabellina = int(tabellina)
+print(f"\nTABELLINA DEL {tabellina}")
+print("-" * 15)
+for i in range(1, 11):
+    risultato = tabellina * i
+    print(f"{tabellina} x {i} = {risultato}")

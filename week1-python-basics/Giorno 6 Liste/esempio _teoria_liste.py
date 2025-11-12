05de9d6198bbdd61e4e4ad658e9a268ead614428
stefano <paciollastefano01@gmail.com>
2025-11-12 16:42:03 +0100

    Add esempio lista giorno 6

---
+++ week1-python-basics/Giorno 6 Liste/esempio _teoria_liste.py
@@ -0,0 +1,11 @@
+# Creiamo una lista
+colori = ["rosso", "verde", "blu", "giallo", "nero"]
+
+# Accediamo agli elementi
+print(f"Primo colore: {colori[0]}")
+print(f"Terzo colore: {colori[2]}")
+print(f"Ultimo colore: {colori[4]}")
+
+# Cosa succede se chiedo colori[5]?
+# print(colori[5])  #Succede che ci da errore se scriviamo questo perchè il conteggio delle liste parte da 0
+
